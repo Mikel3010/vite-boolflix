5e269cec102e929c0f8250fd79a8137b7301aaea
Mikel3010 <mikelgremi30@gmail.com>
2023-04-03 09:49:44 +0200

    milestone3

--- vite-boolflix/src/components/AppCard.vue
+++ vite-boolflix/src/components/AppCard.vue
@@ -1,10 +1,15 @@
 <script>
 import CountryFlag from 'vue-country-flag-next'
 export default{
-    name:'AppCard',
+    name:'AppCard1',
     components:{
         CountryFlag,
 
+    },
+    data(){
+        return{
+            urlImg:'https://image.tmdb.org/t/p/w342',
+        }
     },
     props:{
         info: Object
@@ -19,15 +24,23 @@ export default{
                 return this.info.original_language
             }
         },
+        vote(){
+            return Math.ceil(this.info.vote_average /2);
+        }
     }
 }
 </script>
 <template>
     <div>
+        <div>
+            <img :src="this.urlImg + info.poster_path" alt="">
+        </div>
         <h3>{{ info.title || info.name }}</h3>
         <h4>{{ info.original_title || info.original_name }}</h4>
         <country-flag :country='flagLang' size='small'/>
-        <div>{{ info.vote_average }}</div>
+        <div>{{ vote }}</div>
+        <font-awesome-icon icon="fa-solid fa-star" v-for="n in vote"/>
+        <font-awesome-icon icon="fa-regular fa-star" v-for="n in 5-vote"/>
     </div>
 </template>
 <style lang="scss" scoped></style>
--- vite-boolflix/src/main.js
+++ vite-boolflix/src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import './assets/general.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faStar as faStarfull} from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faStarfull,faStar)
+
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
